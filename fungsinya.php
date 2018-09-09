f683cfe75be7c54e93b738d51e4a2d9e48f4e31b
xsate <biejabbar@gmail.com>
2018-09-09 15:58:19 +0000

    sudah baik work

--- fungsinya.php
+++ fungsinya.php
@@ -1,19 +1,48 @@
 <?php
-require_once("fungsinya.php");
-define("br","\n");
+/*
+Created : Fauzi
+Git Hub	: https://github.com/xsate/warnaterminal
+Title 	: Warna Ansii 
+Support	: Black30, Red31, Green32, Yellow33, Blue34, Purple35, Cyan36, White37 
+*/
 
-if(isset($argv[1])){
-	$kata	= $argv[1];
-	$warna	= $argv[2]??'hitam'; 
-
-}else{
-	echo "Masukkan Kata2nya \n >> ";	
-	$kata 	=  trim(fgets(STDIN));
-
-	echo "Warnanya apa ? \n >> ";
-	$warna	= trim(fgets(STDIN));
+// Format Penulisan "\033[Parameternya Isi String nya \033[0m"
+class WarnaKu
+{
+	public 	
+	       	$kata  = '',
+		$TextColor = 30,
+		$BUI   = '',
+		$cetakWarna = '';
+	function __construct($kata,$warna,$BUI=''){
+		$this->kata = $kata;
+		
+		$this->SetArrayWarna();
+		$this->CariWarna($warna);
+		$this->WarnaKu();
+	}
+	private function SetArrayWarna(){
+		$this->warna = [90 => 'hitam','merah','hijau',
+			 	'kuning','biru','ungu',
+				'cyan', 'putih'];
+	}
+	private function CariWarna($warna){
+		$this->TextColor = array_search($warna,$this->warna,true);	
+	}
+	function WarnaKu(){
+		$warna = "";
+		if($this->BUI != ''){
+			$warna .= $BUI.';' ;	
+		}
+		if($this->TextColor != ''){
+			$warna .= $this->TextColor.'m' ;	
+		}
+		$this->cetakWarna = $warna;
+	}
+	function __tostring(){
+		//$this->warna;		
+		return "\e[".$this->cetakWarna.$this->kata."\e[0m \n";
+	}
 }
-	
-$cetak 	= new WarnaKu($kata, $warna);
-echo $cetak;
+
 ?>
